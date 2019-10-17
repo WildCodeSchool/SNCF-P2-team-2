6bb31c3067557c218215b743faa1ee308fd08557
Koullou Mohamed <koullou.mohamed@gmail.com>
2019-10-17 14:05:00 +0200

    Creation de la navbar sans logo ect..

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Navbar() {
+  return (
+    <div className="mastheader">
+      <header
+        role="banner"
+        className="mastheader-title d-none d-xl-block"
+      ></header>
+    </div>
+  );
+}
+
+export default Navbar;
