c268201d83dac95afbc6e1cc3bf39548764369b8
baouz salim <baouz.salim93240@gmail.com>
2019-10-17 15:10:44 +0200

    new navbar with logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./navbar.css";
+import logo from "./img/logo.png";
 
 function Navbar() {
   return (
     <div className="mastheader">
-      <header
-        role="banner"
-        className="mastheader-title d-none d-xl-block"
-      ></header>
+      <header role="banner" className="mastheader-title d-none d-xl-block">
+        <img src={logo} alt="My logo" />
+      </header>
     </div>
   );
 }
