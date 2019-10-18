da3dbb67f5139ebc1c7cda19472e3154dc10e968
baouz salim <baouz.salim93240@gmail.com>
2019-10-18 10:26:52 +0200

    changement navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,9 +5,11 @@ import logo from "./img/logo.png";
 function Navbar() {
   return (
     <div className="mastheader">
-      <header role="banner" className="mastheader-title d-none d-xl-block">
-        <img src={logo} alt="My logo" />
-      </header>
+      <div className="mastheader-logo">
+        <a href="/docs">
+          <img alt="SNCF" src={logo} />
+        </a>
+      </div>
     </div>
   );
 }
