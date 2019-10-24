5e0e1be56a8eb1de80eed28fb76e48f291c36b98
baouz salim <baouz.salim93240@gmail.com>
2019-10-24 10:05:59 +0200

    Updates

---
+++ precache-manifest.47bb1a2aeca9190167ddb44fa3319696.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ddbdc67a31d94d164c6200cd71b09f8",
+    "url": "/SNCF-P2-team-2/index.html"
+  },
+  {
+    "revision": "6399afb4803b0e3f1342",
+    "url": "/SNCF-P2-team-2/static/css/2.813f4e5f.chunk.css"
+  },
+  {
+    "revision": "16b21212deebbc44e4f8",
+    "url": "/SNCF-P2-team-2/static/css/main.d44e91d9.chunk.css"
+  },
+  {
+    "revision": "6399afb4803b0e3f1342",
+    "url": "/SNCF-P2-team-2/static/js/2.1788cad7.chunk.js"
+  },
+  {
+    "revision": "16b21212deebbc44e4f8",
+    "url": "/SNCF-P2-team-2/static/js/main.601fb2db.chunk.js"
+  },
+  {
+    "revision": "0cef2c45499189e07dbb",
+    "url": "/SNCF-P2-team-2/static/js/runtime-main.52bdc1a6.js"
+  },
+  {
+    "revision": "8e3f2aad85a79f0ca2cc4007dbf6b0ac",
+    "url": "/SNCF-P2-team-2/static/media/Sncf-logo.8e3f2aad.svg"
+  },
+  {
+    "revision": "7a2a255c3e73a1cd7e41162cb23d9497",
+    "url": "/SNCF-P2-team-2/static/media/avenir-black.7a2a255c.woff"
+  },
+  {
+    "revision": "99e41e7f54caf79d6c87a35f8289509e",
+    "url": "/SNCF-P2-team-2/static/media/avenir-book.99e41e7f.woff"
+  },
+  {
+    "revision": "6d2f3cce88d832b3875a18b786e92fe4",
+    "url": "/SNCF-P2-team-2/static/media/avenir-lighter.6d2f3cce.woff"
+  },
+  {
+    "revision": "e1287435947974def7f8b407e28bf542",
+    "url": "/SNCF-P2-team-2/static/media/avenir-medium.e1287435.woff"
+  },
+  {
+    "revision": "b756f2d8ba6eb4ce4f1ec76ccb2414a3",
+    "url": "/SNCF-P2-team-2/static/media/banner.b756f2d8.jpg"
+  },
+  {
+    "revision": "30515d0e4b76636f578fff48a5d8f9d5",
+    "url": "/SNCF-P2-team-2/static/media/icons.30515d0e.eot"
+  },
+  {
+    "revision": "9c615f0698b3d55e2fadb7493e02d4ff",
+    "url": "/SNCF-P2-team-2/static/media/icons.9c615f06.ttf"
+  },
+  {
+    "revision": "be8fb685547e626c1b0a056ec2ba4580",
+    "url": "/SNCF-P2-team-2/static/media/icons.be8fb685.woff2"
+  },
+  {
+    "revision": "c384e4cb8fadcb30b629ac4696c82aa5",
+    "url": "/SNCF-P2-team-2/static/media/icons.c384e4cb.svg"
+  },
+  {
+    "revision": "f340f41f1d5fb6442ed0a1fca021373f",
+    "url": "/SNCF-P2-team-2/static/media/icons.f340f41f.woff"
+  }
+]);
