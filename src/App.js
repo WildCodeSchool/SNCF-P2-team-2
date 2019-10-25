ade57b6a6f877f37a4173821445d978643124713
AudCor <clapyourhands4ever@gmail.com>
2019-10-25 12:44:57 +0200

    Ajout temporaire du composant ResultItineraryFormatting dans l'App.js pour vérifier le bon affichage du composant

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import "@sncf/bootstrap-sncf.metier/dist/bootstrap-sncf";
 import Navbar from "./components/Navbar/Navbar";
 import BannerUnderNav from "./components/BannerUnderNav/BannerUnderNav";
 import Card from "./components/CardItinerary/Card";
+import ResultItineraryFormatting from "./components/ResultItineraryFormatting/ResultItineraryFormatting"; // temporaire
 import "./styles.css";
 
 function App() {
-	return (
-		<div>
-			<Navbar />
-			<BannerUnderNav />
-			<Card />
-			<main role="main" className="mastcontainer"></main>
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+      <BannerUnderNav />
+      <Card />
+      <main role="main">
+        <ResultItineraryFormatting /> {/* Temporaire */}
+      </main>
+    </div>
+  );
 }
 
 export default App;
