870c48596057933090b79568d76c1a3937220f4d
AudCor <clapyourhands4ever@gmail.com>
2019-10-25 15:21:27 +0200

    fix conflicts

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import Navbar from "./components/Navbar/Navbar";
 import BannerUnderNav from "./components/BannerUnderNav/BannerUnderNav";
 import Card from "./components/CardItinerary/Card";
 import "./styles.css";
-import ResultItineraryInfos from './components/ResultItineraryInfos/ResultItineraryInfos';
+import ResultItineraryInfos from "./components/ResultItineraryInfos/ResultItineraryInfos";
 
 function App() {
-	return (
-		<div>
-			<Navbar />
-			<BannerUnderNav />
-			<main role="main">			
-			<Card />
-			<ResultItineraryInfos />
-			</main>
-
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+      <BannerUnderNav />
+      <main role="main">
+        <Card />
+        <ResultItineraryInfos />
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/components/CardItinerary/Card.js
+++ src/components/CardItinerary/Card.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./card.css";
+import SelectDepArr from "../SelectDepArr/SelectDepArr";
 
 function Card() {
-	return (
-		<div className="row ">
-			<div className="col-sm-12 col-md-12 col-lg-8  mx-auto p-5 card-itinerary">
-				<div className="bg-white mb-3 rounded p-5 card-shadow">
-					<h2 className="text-uppercase">Itinéraire</h2>
-					<p>
-						
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="row ">
+      <div className="col-sm-12 col-md-12 col-lg-8  mx-auto p-5 card-itinerary">
+        <div className="bg-white mb-3 rounded p-5 card-shadow">
+          <h2 className="text-uppercase">Itinéraire</h2>
+
+          <SelectDepArr />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
