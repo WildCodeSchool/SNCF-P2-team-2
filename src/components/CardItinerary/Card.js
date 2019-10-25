378749f8220e3190d76b078a52ebfb8515889706
Hadibéré <hadibere@gmail.com>
2019-10-25 16:04:48 +0200

    ajout du formulaire autocompletion

--- src/components/CardItinerary/Card.js
+++ src/components/CardItinerary/Card.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./card.css";
+import ItinerarySubmitButton from "./ItinerarySubmitButton/ItinerarySubmitButton";
 import InputSearchDeparture from "../InputSearchDeparture/InputSearchDeparture";
 
-function Card(props) {
+function Card() {
 	return (
 		<div className="row ">
-			<div className="col-sm-12 col-md-10 col-lg-10  mx-auto p-5 card-itinerary">
+			<div className="col-sm-12 col-md-12 col-lg-8  mx-auto p-5 card-itinerary">
 				<div className="bg-white mb-3 rounded p-5 card-shadow">
 					<h2 className="text-uppercase">Itinéraire</h2>
 					<InputSearchDeparture />
-					{props.hello}
+					<ItinerarySubmitButton />
 				</div>
 			</div>
 		</div>
