a6f96dcde87c518c787dc67a6648da481d0198a8
Camara Hadibéré <hadibere@gmail.com>
2019-11-04 17:34:48 +0100

    Modification est mise forme de la card

--- src/components/CardItinerary/Card.js
+++ src/components/CardItinerary/Card.js
@@ -7,35 +7,37 @@ import SelectDepArr from "../SelectDepArr/SelectDepArr";
 import Timepickerform from "../Timeselectform";
 
 function Card() {
-  return (
-    <div className="row ">
-      <div className="col-sm-12 col-md-12 col-lg-10  mx-auto card-itinerary">
-        <div className="bg-white mb-3 rounded px-4 py-5 card-shadow">
-          <h2 className="text-uppercase">Itinéraire</h2>
-          <div className="row mb-5">
-            <div className="col">
-              <InputSearchDeparture />
-            </div>
-            <div className="col">
-              <InputSearchDeparture />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <SelectDepArr />
-            </div>
-            <div className="col">
-              <DatePickerr />
-              <Timepickerform />
-            </div>
-            <div className="col">
-              <ItinerarySubmitButton />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="row ">
+			<div className="col-sm-12 col-md-12 col-lg-10  mx-auto card-itinerary">
+				<div className="bg-white mb-3 rounded px-4 py-5 card-shadow">
+					<h2 className="text-uppercase">Itinéraire</h2>
+					<div className="row mb-5">
+						<div className="col">
+							<InputSearchDeparture />
+						</div>
+						<div className="col">
+							<InputSearchDeparture />
+						</div>
+					</div>
+					<div className="row">
+						<div className="col">
+							<SelectDepArr />
+						</div>
+						<div className="col">
+							<DatePickerr />
+						</div>
+						<div className="col">
+							<Timepickerform />
+						</div>
+						<div className="col">
+							<ItinerarySubmitButton />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Card;
