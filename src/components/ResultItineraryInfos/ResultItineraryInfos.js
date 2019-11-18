36fb8c5d840420c0b03ab36235b1eec0ceacbf20
AudCor <clapyourhands4ever@gmail.com>
2019-11-18 13:34:20 +0100

    Affichage message erreur dans de cas où l'API ne fournit aucun résultat

--- src/components/ResultItineraryInfos/ResultItineraryInfos.js
+++ src/components/ResultItineraryInfos/ResultItineraryInfos.js
@@ -7,25 +7,34 @@ import {
   dataTransportFormatting
 } from "../DataAPIFormatting/DataAPIFormatting";
 import ResultItineraryFormatting from "../ResultItineraryFormatting/ResultItineraryFormatting";
+import "./errorMessage.css";
 
 const ResultItineraryInfos = ({ globalState }) => {
-  return globalState.result.map(res => {
-    return (
-      <ResultItineraryFormatting
-        key={uuid()}
-        depTime={dataTimeFormatting(res.departure_date_time)}
-        arrTime={dataTimeFormatting(res.arrival_date_time)}
-        durationJourney={dataDurationFormatting(res.durations.total)}
-        durationWalking={
-          res.durations.walking > 0
-            ? dataDurationFormatting(res.durations.walking)
-            : ""
-        }
-        transportJourney={dataTransportFormatting(res.sections)}
-        price={res.fare.found ? dataPriceFormatting(res.fare.total.value) : "-"}
-      />
-    );
-  });
+  return globalState.result ? (
+    globalState.result.map(res => {
+      return (
+        <ResultItineraryFormatting
+          key={uuid()}
+          depTime={dataTimeFormatting(res.departure_date_time)}
+          arrTime={dataTimeFormatting(res.arrival_date_time)}
+          durationJourney={dataDurationFormatting(res.durations.total)}
+          durationWalking={
+            res.durations.walking > 0
+              ? dataDurationFormatting(res.durations.walking)
+              : ""
+          }
+          transportJourney={dataTransportFormatting(res.sections)}
+          price={
+            res.fare.found ? dataPriceFormatting(res.fare.total.value) : "-"
+          }
+        />
+      );
+    })
+  ) : (
+    <div className="errorMsg">
+      <p>Aucun trajet disponible</p>
+    </div>
+  );
 };
 
 export default ResultItineraryInfos;
